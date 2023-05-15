0a3502f72e0b4d414bb05849aa221c20dc46b491
habazaki2001 <85531602+habazaki2001@users.noreply.github.com>
2023-05-15 15:04:43 +0700

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
 import * as React from 'react';
+import { useState } from "react";
 import './style.css';
 
 export default function App() {
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const dataJobsStorage = JSON.parse(localStorage.getItem('jobs'))
+    return dataJobsStorage ?? []
+  })
+
+  const handleSubmit = (job) => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      const converdJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', converdJobs)
+      return newJobs
+    })
+    setJob('')
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Todolist Jobs with useState and save LocalStorage</h1>
+      <input type="text" value={job} onChange={e => setJob(e.target.value)}/>
+      <ul>
+        {
+          jobs.map((item, index) => (
+              <li
+                key={index}
+              >
+                {item}
+              </li>
+          ))
+        }
+      </ul>
+      <button onClick={() => handleSubmit(job)} >Add</button>
     </div>
   );
 }
